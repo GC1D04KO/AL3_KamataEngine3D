fa18a0bade3691905c8300473e37f84d5684c5e1
GC1D04KO <k023g1067@g.neec.ac.jp>
2024-07-23 14:09:51 +0900

    Squashed commit of the following:
    commit 314f773a4d0380423bf09a98145e15b13164dd44
    Author: GC1D04KO <k023g1067@g.neec.ac.jp>
    Date:   Tue Jul 23 13:58:19 2024 +0900
    
        02-11の確認課題
    
    commit 92ff45b5dfe12fc275d11f279f9a1d0dea76c803
    Author: GC1D04KO <k023g1067@g.neec.ac.jp>
    Date:   Tue Jul 23 11:03:38 2024 +0900
    
        Squashed commit of the following:
    
        commit dc9d662f7c340641e73694a24614fa6c3d878c72
        Author: GC1D04KO <k023g1067@g.neec.ac.jp>
        Date:   Tue Jul 23 10:46:29 2024 +0900
    
            02-10の確認課題
    
        commit fb3962c9bb894942096cd840187392deafd116d1
        Author: GC1D04KO <k023g1067@g.neec.ac.jp>
        Date:   Tue Jul 23 10:38:46 2024 +0900
    
            02-10の確認課題
    
        commit 75a68c2ec6a852ecf6830e4e08bbc25959e38b14
        Author: GC1D04KO <k023g1067@g.neec.ac.jp>
        Date:   Sat Jul 20 17:00:25 2024 +0900
    
            Squashed commit of the following:
    
            commit 8a1fb1e644638b6190975d2aa13c3f57ef4948b1
            Author: GC1D04KO <k023g1067@g.neec.ac.jp>
            Date:   Sat Jul 20 16:58:56 2024 +0900
    
                02-09の確認課題
    
            commit cb391c9643e071e03a0c4d674362b2d06a536948
            Author: GC1D04KO <k023g1067@g.neec.ac.jp>
            Date:   Sat Jul 20 16:48:04 2024 +0900
    
                Squashed commit of the following:
    
                commit fa5995ac9b9c2c2be3023caed5f72dd1ba91092f
                Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                Date:   Sat Jul 20 16:44:36 2024 +0900
    
                    02-08の確認課題
    
                commit 67ff43c5cd0a3556ee70cf9d0d0b1edba277f70e
                Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                Date:   Sat Jul 20 16:32:10 2024 +0900
    
                    Squashed commit of the following:
    
                    commit fcdf6c24d7000d09787a7183cc0d772e7d3cb71d
                    Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                    Date:   Sat Jul 20 16:02:25 2024 +0900
    
                        02-07の確認課題
    
                    commit 6b666826bbd1651da420c4c01b49e5ae832d4897
                    Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                    Date:   Sat Jul 20 15:13:58 2024 +0900
    
                        Squashed commit of the following:
    
                        commit bbda642612395dc6fc63b2028d4b6d3070f1745f
                        Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                        Date:   Sat Jul 20 15:12:27 2024 +0900
    
                            02-06の確認課題
    
                        commit a981112dc5eb4a6548c0d82aa04ba7c09fd2a258
                        Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                        Date:   Sat Jul 20 14:45:12 2024 +0900
    
                            Squashed commit of the following:
    
                            commit 61be6c224b2bb9a55616f17f4e8ffa02c04813c3
                            Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                            Date:   Sat Jul 20 14:17:53 2024 +0900
    
                                02-05の確認課題
    
                            commit 1fce0a0f65a94b48b3bf6427e155802f13c1353a
                            Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                            Date:   Tue Jul 16 10:36:56 2024 +0900
    
                                Create block.png
    
                            commit 7ddb9839f98e89842bbfb4009e68727d1bdca471
                            Author: GC1D04KO <k023g1067@g.neec.ac.jp>
                            Date:   Fri Jul 12 15:00:31 2024 +0900
    
                                02_04の確認課題

--- DirectXGame/scene/GameScene.h
+++ DirectXGame/scene/GameScene.h
@@ -7,8 +7,15 @@
 #include "Sprite.h"
 #include "ViewProjection.h"
 #include "WorldTransform.h"
-
-/// <summary>
+#include <vector>
+#include "Player.h"
+#include "DebugCamera.h"
+#include "MapChipField.h"
+#include "CameraController.h"
+#include "Skydome.h"
+#include "Enmey.h"
+#include "list"
+#include"DeathParticles.h"
 /// ゲームシーン
 /// </summary>
 class GameScene {
@@ -40,9 +47,60 @@ public: // メンバ関数
 	void Draw();
 
 private: // メンバ変数
+
+	enum class Phase {
+		kPlay,//ゲームプレイヤ
+		kDeath//デスの演出+-09
+	};
+
 	DirectXCommon* dxCommon_ = nullptr;
 	Input* input_ = nullptr;
 	Audio* audio_ = nullptr;
+
+	/// <summary>
+	/// ゲームシーン用
+	/// </summary>
+	// ビュープロジェクション
+	ViewProjection viewProjection_;
+	// テクスチャハンドル
+	uint32_t textureHandle_ = 0;
+	// 自キャラ
+	Player* player_ = nullptr;
+	Enemy* enemy_ = nullptr;
+	// モデルデータ
+	Model* modelPlayer_ = nullptr;
+	Model* modelBlock_ = nullptr;
+	Model* modelSkydome_ = nullptr;
+	Model* modelEnemy_ = nullptr;
+	Model* modelDeathParticle_ = nullptr;
+	std::vector<std::vector<WorldTransform*>> worldTransformBlocks_;
+	WorldTransform worldTransformSkydome_;
+	// デバッグカメラ
+	DebugCamera* debugCamera_ = nullptr;
+	// デバッグカメラ有効
+	bool isDebugCameraActive_ = false;
+	// マップチップフィールド
+	MapChipField* mapChipField_;
+	CameraController* cameraController = nullptr;
+
+	std::list<Enemy*> enemies_;
+	Enemy* newEnemy_ = nullptr;
+
+	Phase phase_;
+
+	DeathParticles* deathParticles_ = nullptr;
+
+	void ChangePhase();
+
+	void GenerateBlocks();
+
+	void UpdateCamera();
+
+	void UpdateBlocks();
+
+	// 衝突判定と応答
+
+	void CheckAllCollisions();
 
 	/// <summary>
 	/// ゲームシーン用
